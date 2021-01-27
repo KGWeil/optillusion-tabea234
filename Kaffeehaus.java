f0890ac438c7a83b4716934e6aa3a0ad0cdb996e
Tabea <silke.propfe@t-online.de>
2021-01-27 18:32:15 +0100

    kaffehaus

--- Kaffeehaus.java
+++ Kaffeehaus.java
@@ -3,13 +3,13 @@ import processing.core.PApplet;
 import processing.core.PFont;
 
 /**
- * Klasse zeichneQuadrate.
+ * Klasse Kaffeehaus.
  * Beschreibung: 
  *
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class zeichneQuadrate extends PApplet
+public class Kaffeehaus extends PApplet
 {       
     /**
      * settings() Methode 
@@ -18,7 +18,7 @@ public class zeichneQuadrate extends PApplet
     @Override
     public void settings()
     {
-        size(600,600);
+        size(640,400);
     }        
 
     /**
@@ -27,37 +27,43 @@ public class zeichneQuadrate extends PApplet
      * und Medien wie Bilder und Schriftarten geladen.
      */
     
-    public void zeichneQuadrate(){
-     for (int i=0; i<10;i++)
-     rect (50+i*25,50,25,25);
-    }
-    
-    
-    public void zeichneKreise(){
-    for(int i=0;i<20;i++ )
-    ellipse(300,300,500-i*25,500-i*25);
+    public void setup()
+    {
+    background( 255 ); //Hintergrund weiß
+    parallelen(); //zeichnen der parallelen
+    zeichneAlleQuadrate(); //zeichnen der Quadrate
     }
 
-    
-    
     /**
      * Die draw() Methode wird nach der setup() Methode aufgerufen
      * und führt den Code innerhalb ihres Blocks fortlaufend aus,
      * bis das Programm gestoppt oder noLoop() aufgerufen wird.
      */
-    @Override
-    public void setup()
+    
+    public void parallelen()
     {
-    //zeichneQuadrate();
-    zeichneKreise();
+    stroke (125); //Linienfarbe
+    int s=40;
+    for(int i=0;i<10;i++)
+    line (0,20+i*s,640,20+i*s);
     }
-
+    public void zeichneAlleQuadrate() {
+    int s=40;
+    int[] abstaende={10,20,30,20,10,20,30,20,10};
+    fill (0); //Farbe
+       for(int i=0;i<8;i++){
+            for (int j=0;j<8;j++){
+    rect (abstaende[j]+i*s*2,20+s*j,s,s);
+            }
+       }
+    }
+    
     /**
      * Mit der main()-Methode wird das Programm gestartet.
      *
      */    
     public static void main(String _args[]){ 
-        PApplet.main(new String[] {zeichneQuadrate.class.getName() });
+        PApplet.main(new String[] {Kaffeehaus.class.getName() });
     }
 
 }
